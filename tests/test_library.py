718e644b4fa67090061905929967ca8a3fe945dd
Kayla Van Bortel <kvanbortel@gmx.com>
2024-01-18 19:31:40 -0500

    add library and test

---
+++ tests/test_library.py
@@ -0,0 +1,18 @@
+import unittest
+from src.library import *
+from src.swen344_db_utils import connect
+
+class TestLibrary(unittest.TestCase):
+
+    def test_rebuild_tables(self):
+        """Rebuild the tables"""
+        rebuildTables()
+        result = exec_get_all('SELECT * FROM example_table')
+        self.assertEqual([], result, "no rows in example_table")
+
+    def test_rebuild_tables_is_idempotent(self):
+        """Drop and rebuild the tables twice"""
+        rebuildTables()
+        rebuildTables()
+        result = exec_get_all('SELECT * FROM example_table')
+        self.assertEqual([], result, "no rows in example_table")
